936b5e08f3d3533406c4846a46d85e24740758f9
Dominykas Stunžėnas <dominykas.stunzenas@stud.vilniustech.lt>
2024-06-07 11:44:54 +0300

    project setup

---
+++ games-app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.6"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.project"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ games-app/src/test/java/com/project/games_app/GamesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.games_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
